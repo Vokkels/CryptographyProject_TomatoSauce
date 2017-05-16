d4b0b3c6f68d3de8f879610764d8f484a25f6f85
Schalk <pretorius.scw@gmail.com>
2017-05-16 14:16:32 +0200

    Added wrong input correction Added warning messages

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -16,6 +16,10 @@ import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 
+/**
+ * Main class for communication  between the interface and activities
+ * @author Schalk Pretorius </pretorius.scw@gmail.com>
+ */
 public class Controller {
 
     public static CryptoMain cm;
@@ -25,6 +29,9 @@ public class Controller {
     private boolean threadStarted;
     public static boolean useNormalASCII;
 
+    /**
+     * The @FXML annotation an FMXLLoader to inject values in an FXML file reference.
+     */
     @FXML
     private ToggleButton tglType;
     @FXML
@@ -52,13 +59,47 @@ public class Controller {
         return location;
     }
 
-    public void encryptFile(ActionEvent actionEvent) {
-        runCipher(true);}
+    /**
+     *
+     * @param actionEvent
+     */
+    public void encryptFile(ActionEvent actionEvent)
+    {
+        if (tglType.isSelected() && inputTxt.getText() == null)
+            ErrorMessage("TypeSomething", null);
+        else if (tglFile.isSelected() && location == "")
+            ErrorMessage("DropSomething", null);
+        else if (!tglFile.isSelected() && !tglType.isSelected())
+            ErrorMessage("SelectSomething", null);
+        else if (inputKey.getText() == null || inputKey.getText().length() < 8)
+            ErrorMessage("TypePassKey", null);
+        else
+            runCipher(true);
 
-    public void decryptFile(ActionEvent actionEvent) {
-        runCipher(false);
     }
 
+    /**
+     *
+     * @param actionEvent
+     */
+    public void decryptFile(ActionEvent actionEvent)
+    {
+        if (tglType.isSelected() && inputTxt.getText() == null)
+            ErrorMessage("TypeSomething", "decrpyt");
+        else if (tglFile.isSelected() && location == "")
+            ErrorMessage("DropSomething", "decrpyt");
+        else if (!tglFile.isSelected() && !tglType.isSelected())
+            ErrorMessage("SelectSomething", "decrpyt");
+        else if (inputKey.getText() == null || inputKey.getText().length() < 8)
+            ErrorMessage("TypePassKey", "decrpyt");
+        else
+            runCipher(false);
+    }
+
+    /**
+     *
+     * @return
+     */
     public encryptionType radioButtons() {
         if (transposition.isSelected()) {
             useNormalASCII = true;
@@ -79,6 +120,10 @@ public class Controller {
         return null;
     }
 
+    /**
+     *
+     * @param event
+     */
     public void dropping(DragEvent event) {
         Dragboard db = event.getDragboard();
         boolean success = false;
@@ -93,6 +138,10 @@ public class Controller {
         System.out.println(location);
     }
 
+    /**
+     *
+     * @param event
+     */
     public void draggingOver(DragEvent event) {
         Dragboard db = event.getDragboard();
         if (db.hasFiles()) {
@@ -115,6 +164,10 @@ public class Controller {
         inputTxt.setText(message);
     }
 
+    /**
+     *
+     * @param encrypt
+     */
     public void runCipher(boolean encrypt) {
 
         progress = 0;
@@ -145,12 +198,15 @@ public class Controller {
     }
 
     public void TypeMSG(MouseEvent mouseEvent) {
+        inputTxt.setText(null);
+        inputKey.setText(null);
         inputTxt.promptTextProperty().setValue("                      Type a message.");
         inputTxt.editableProperty().setValue(true);
         inputTxt.setAlignment(Pos.CENTER_LEFT);
     }
 
     public void SelFILE(MouseEvent mouseEvent) {
+        inputKey.setText(null);
         inputTxt.promptTextProperty().setValue("                    Drag and Drop File.");
         inputTxt.editableProperty().setValue(false);
         inputTxt.textProperty().setValue("");
@@ -162,5 +218,51 @@ public class Controller {
             inputTxt.setAlignment(Pos.TOP_LEFT);
         else if (tglFile.isSelected())
             inputTxt.setAlignment(Pos.CENTER_LEFT);
+    }
+
+    public void ErrorMessage(String whatMessage, String decrypt)
+    {
+        if (whatMessage == "TypeSomething") {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText(null);
+            if (decrypt == null)
+                alert.setContentText("There is no message to encrypt. \nPlease type something in the designated area.");
+            else
+                alert.setContentText("There is no message to decrypt. \nPlease type something in the designated area.");
+            alert.showAndWait();
+        }
+
+        if (whatMessage == "DropSomething")
+        {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText(null);
+            if (decrypt == null)
+                alert.setContentText("There is currently no file selected to encrypt. \nPlease drag and drop a file into the designated area.");
+            else
+                alert.setContentText("There is currently no file selected to decrypt. \nPlease drag and drop a file into the designated area.");
+            alert.showAndWait();
+        }
+
+        if (whatMessage == "TypePassKey") {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText(null);
+            alert.setContentText("Please type a passkey longer than 8 characters to continue.");
+            alert.showAndWait();
+        }
+
+        if (whatMessage == "SelectSomething")
+        {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Attention");
+            alert.setHeaderText(null);
+            if (decrypt == null)
+                alert.setContentText("There is currently no option selected to perform any encryption on.");
+            else
+                alert.setContentText("There is currently no option selected to perform any decryption on.");
+            alert.showAndWait();
+        }
     }
 }
